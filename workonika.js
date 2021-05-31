20a98df69677a1e9fce9c0798f4d80b59c2ad06f
Кузьмин Антон Геннадьевич <kuzmin.a.gennadye@sberbank.ru>
2021-05-31 23:30:04 +0300

    Добавил шаблоны handlebars

--- workonika.js
+++ workonika.js
@@ -1,25 +1,23 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
 
 const app = express()
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Workonika survey')
-})
+app.get('/', (req, res) => res.render('home'))
 
 app.use((req, res) => {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - не найдено')
+    res.render('404')
 })
 
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - ошибка сервера')
+    res.render('500')
 })
 
 app.listen(port, () => console.log(`Express запущен на http://localhost:${port}; нажмите Ctrl + C для завершения`))
