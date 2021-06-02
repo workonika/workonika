81d84f5e4d2f19adc87b8903c9d7de7aabf9dd9a
Кузьмин Антон Геннадьевич <kuzmin.a.gennadye@sberbank.ru>
2021-06-02 22:48:07 +0300

    Тестовая страница send

--- workonika.js
+++ workonika.js
@@ -32,6 +32,16 @@ const tours = [
 ]
 app.get('/api/tours', (req, res) => res.json(tours))
 
+
+app.put('/api/tour/:id', (req, res) => {
+    const p = tours.find(p => p.id === parseInt(req.params.id))
+    if (!p) {return res.status}
+    if(!p) return res.status(404).json({ error: 'No such tour exists' })
+    if(req.body.name) p.name = req.body.name
+    if(req.body.price) p.price = req.body.price
+    res.json({ success: true })
+})
+
 app.use((req, res) => {
     res.status(404)
     res.render('404')
